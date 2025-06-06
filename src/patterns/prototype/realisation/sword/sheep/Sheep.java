9bf9a25fd7c19a965765a0a0ae4bd98025b34c5a
morozvitali <morozvitali99@gmail.com>
2025-06-06 12:35:59 +0300

    [PROTOTYPE] realisation 2.00

--- src/patterns/prototype/realisation/sword/sheep/Sheep.java
+++ src/patterns/prototype/realisation/sword/sheep/Sheep.java
@@ -1,4 +1,29 @@
 package patterns.prototype.realisation.sword.sheep;
 
-public class Sheep {
+public class Sheep implements Cloneable {
+    private String name;
+
+    public Sheep(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public Sheep clone() {
+        try {
+            return (Sheep) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError();
+        }
+    }
+
+    public void sayHello() {
+        System.out.println("бебебе мене звати " + name);
+    }
+
+    public static void main(String[] args) {
+        Sheep sheep1 = new Sheep("бебе1");
+        Sheep sheep2 = sheep1.clone();
+        sheep1.sayHello();
+        sheep2.sayHello();
+    }
 }

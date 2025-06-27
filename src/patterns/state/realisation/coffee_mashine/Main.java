ab84073a5a6c9a33d24c5f2474d643cd9801d747
morozvitali <morozvitali99@gmail.com>
2025-06-27 16:38:27 +0300

    [STATE] Realisation Task 2.01

---
+++ src/patterns/state/realisation/coffee_mashine/Main.java
@@ -0,0 +1,13 @@
+package patterns.state.realisation.coffee_mashine;
+
+public class Main {
+    public static void main(String[] args) {
+        CoffeeMashine mashine = new CoffeeMashine();
+    mashine.pressButton();
+    mashine.pressButton();
+    mashine.pressButton();
+    mashine.pressButton();
+    mashine.pressButton();
+    mashine.pressButton();
+    }
+}
